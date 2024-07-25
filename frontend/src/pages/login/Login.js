054cf856b7e087146929c56320a1736dfb06d125
heejinssss <heejin.dev33@gmail.com>
2024-07-25 23:37:26 +0900

    pbs-37--feat: Header 구현

---
+++ frontend/src/pages/login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/atoms/Header';
+
+function Login() {
+    console.log('***Login Page Rendered***');
+
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
+
+export default Login;
