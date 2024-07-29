55f6e8a2ca03e63ae7511a8eb17470f517595ea6
heejinssss <heejin.dev33@gmail.com>
2024-07-29 17:21:02 +0900

    pbs-49--feat: 메인 화면 템플릿

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRoutes } from 'react-router';
 
+import { Intro } from './pages/intro';
 import { Login } from './pages/login';
 import { Join } from './pages/join';
 import { MyAsk } from './pages/myask';
@@ -9,6 +10,7 @@ import { Collaboration } from './pages/collaboration';
 
 function App() {
     return useRoutes([
+        { path: '/', element: <Intro /> }, // 메인 화면
         { path: '/login', element: <Login /> },
         { path: '/join', element: <Join /> },
         { path: '/myask', element: <MyAsk /> },
