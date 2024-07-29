2e21734c974910eb92ba8777607395878c482e6a
mmmtobezip <mmmszip@gmail.com>
2024-07-29 11:06:00 +0900

    poscopjt-42--chore: backend 환경 구축

---
+++ backend/inquiry-system/src/main/java/com/pobluesky/inquiry_system/InquirySystemApplication.java
@@ -0,0 +1,13 @@
+package com.pobluesky.inquiry_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquirySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquirySystemApplication.class, args);
+	}
+
+}
---
+++ backend/inquiry-system/src/test/java/com/pobluesky/inquiry_system/InquirySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pobluesky.inquiry_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquirySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
