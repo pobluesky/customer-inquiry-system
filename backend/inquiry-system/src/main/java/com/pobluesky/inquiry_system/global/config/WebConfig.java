3c1ef2ed0dc8e19a494905c35acfaebe5820e20f
Yejin Shin <93516595+born-A@users.noreply.github.com>
2024-07-30 13:44:08 +0900

    Revert "BE chore#48 syj  도메인 구조, BaseEntity"

--- backend/inquiry-system/src/main/java/com/pobluesky/inquiry_system/global/config/WebConfig.java
+++
@@ -1,4 +0,0 @@
-package com.pobluesky.inquiry_system.global.config;
-
-public class WebConfig {
-}
--- backend/inquiry-system/src/main/java/com/pobluesky/inquiry_system/global/error/CommonException.java
+++
@@ -1,4 +0,0 @@
-package com.pobluesky.inquiry_system.global.error;
-
-public class CommonException {
-}
