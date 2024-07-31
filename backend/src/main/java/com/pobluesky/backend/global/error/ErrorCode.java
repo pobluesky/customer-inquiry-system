0d37ae5365e77b151fb6605de3fc7124891dabf7
Jiyoongrace <giyun3632@naver.com>
2024-07-31 17:56:23 +0900

    pbs-77--feat: Quality + QualityReviewInfo ErrorCode

--- backend/src/main/java/com/pobluesky/backend/global/error/ErrorCode.java
+++ backend/src/main/java/com/pobluesky/backend/global/error/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
     UNKNOWN(HttpStatus.INTERNAL_SERVER_ERROR, "G0001", "알 수 없는 오류가 발생했습니다."),
     INVALID_REQUEST(HttpStatus.INTERNAL_SERVER_ERROR, "G0002", "잘못된 요청입니다."),
     EXTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G0003", "외부 서버 오류입니다."),
-    USER_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "U0001", "존재하지 않는 사용자입니다.");
+    USER_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "U0001", "존재하지 않는 사용자입니다."),
+    QUALITY_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "Q0001", "존재하지 않는 품질입니다."),
+    QUALITY_REVIEW_INFO_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "Q0002", "존재하지 않는 품질검토정보입니다.");
 
     private HttpStatus status;
     private String code;
