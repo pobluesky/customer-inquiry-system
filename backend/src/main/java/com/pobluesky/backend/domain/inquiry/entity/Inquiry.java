c26d1b507208f3192b1e6c401c7037ab8897f483
parlyresk <parlyresk325@gmail.com>
2024-07-31 19:13:32 +0900

    pbs-73--feacchore: onetomany 관계 설정

--- backend/src/main/java/com/pobluesky/backend/domain/inquiry/entity/Inquiry.java
+++ backend/src/main/java/com/pobluesky/backend/domain/inquiry/entity/Inquiry.java
@@ -3,9 +3,12 @@ package com.pobluesky.backend.domain.inquiry.entity;
 
 import com.pobluesky.backend.domain.user.entity.Customer;
 import com.pobluesky.backend.domain.user.entity.Department;
+import com.pobluesky.backend.domain.user.entity.Manager;
+import com.pobluesky.backend.domain.user.entity.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -16,6 +19,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Getter
@@ -28,7 +32,13 @@ public class Inquiry {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long inquiryNo;
 
-    private long userNo;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_no")
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manager_no")
+    private Manager manager;
 
     @Enumerated(EnumType.STRING)
     private Country country;
@@ -58,7 +68,8 @@ public class Inquiry {
 
     private String elapsedDays;
 
-    private String corporationCode="(주) 포스코";
+    @ColumnDefault("'(주) 포스코'")
+    private String corporationCode;
 
     private String files;
 
