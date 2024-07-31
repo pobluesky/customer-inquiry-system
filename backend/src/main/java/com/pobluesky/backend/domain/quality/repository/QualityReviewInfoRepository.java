dfba080a717c75abf2587c48546d9b27881abee4
Jiyoongrace <giyun3632@naver.com>
2024-07-31 17:57:42 +0900

    pbs-77--feat: Quality + QualityReviewInfo Repository

---
+++ backend/src/main/java/com/pobluesky/backend/domain/quality/repository/QualityReviewInfoRepository.java
@@ -0,0 +1,25 @@
+package com.pobluesky.backend.domain.quality.repository;
+
+import com.pobluesky.backend.domain.quality.entity.QualityReviewInfo;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class QualityReviewInfoRepository {
+
+    public List<QualityReviewInfo> findAll() {
+        return null;
+    }
+
+    public QualityReviewInfo save(QualityReviewInfo qualityReviewInfo) {
+        return null;
+    }
+
+    public Optional<Object> findById(Long qualityReviewInfoNo) {
+        return null;
+    }
+
+    public void deleteById(Long qualityReviewInfoNo) {
+    }
+}
