5575e855601ff123b536dab7665fb65e088f963a
heejinssss <heejin.dev33@gmail.com>
2024-07-31 10:35:24 +0900

    pbs-65--refactor: import문 삭제

--- frontend/src/pages/inq/Inq.js
+++ frontend/src/pages/inq/Inq.js
@@ -1,18 +1,10 @@
-import '@toast-ui/editor/dist/toastui-editor.css';
-import { Editor } from '@toast-ui/react-editor';
 import Header from '../../components/atoms/Header';
 
 function Inq() {
     return (
         <div>
             <Header />
-            <Editor
-                initialValue="요청 사항을 입력하세요."
-                previewStyle="vertical"
-                height="300px"
-                initialEditType="markdown"
-                useCommandShortcut={true}
-            />;
+            We need Inq Text Editor in here.
         </div>
     )
 }
