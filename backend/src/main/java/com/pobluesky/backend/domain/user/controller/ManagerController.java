9de9206597b1d6911158cf19ca21fc9b74df0f98
born-A <93516595+born-A@users.noreply.github.com>
2024-07-31 14:02:05 +0900

    pbs-70--feat: user controller

---
+++ backend/src/main/java/com/pobluesky/backend/domain/user/controller/ManagerController.java
@@ -0,0 +1,65 @@
+package com.pobluesky.backend.domain.user.controller;
+
+import com.pobluesky.backend.domain.user.dto.request.ManagerCreateRequestDTO;
+import com.pobluesky.backend.domain.user.dto.request.ManagerUpdateRequestDTO;
+import com.pobluesky.backend.domain.user.dto.response.ManagerResponseDTO;
+import com.pobluesky.backend.domain.user.service.ManagerService;
+import com.pobluesky.backend.global.util.ResponseFactory;
+import com.pobluesky.backend.global.util.model.CommonResult;
+import com.pobluesky.backend.global.util.model.JsonResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/managers")
+public class ManagerController {
+    private final ManagerService managerService;
+
+    @GetMapping
+    @PostMapping
+    public ResponseEntity<JsonResult> getUsers() {
+        List<ManagerResponseDTO> allUsers = managerService.getAllUsers();
+
+        return ResponseEntity.status(HttpStatus.OK)
+            . body(JsonResult.success(allUsers));
+    }
+
+    @PostMapping
+    public ResponseEntity<JsonResult> createUser(@RequestBody ManagerCreateRequestDTO dto) {
+        ManagerResponseDTO response = managerService.createUser(dto);
+
+        return ResponseEntity.status(HttpStatus.OK)
+            . body(JsonResult.success(response));
+    }
+
+    @PutMapping("/{userNo}")
+    public ResponseEntity<JsonResult> updateUserByNo(
+        @PathVariable Long userNo,
+        @RequestBody ManagerUpdateRequestDTO customerUpdateRequestDTO
+    ) {
+        ManagerResponseDTO response = managerService.updateUserByNo(userNo, customerUpdateRequestDTO);
+
+        return ResponseEntity.status(HttpStatus.OK)
+            . body(JsonResult.success(response));
+    }
+
+    @DeleteMapping("/{userNo}")
+    public ResponseEntity<CommonResult> deleteUserByNo(@PathVariable Long userNo) {
+        managerService.deleteUserByNo(userNo);
+
+        return ResponseEntity.ok(ResponseFactory.getSuccessResult());
+    }
+
+}
