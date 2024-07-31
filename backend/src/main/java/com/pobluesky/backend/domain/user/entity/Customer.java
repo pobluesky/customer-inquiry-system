e3c9492a81a7db9bb749fbaf6a034496bd94d0fc
born-A <93516595+born-A@users.noreply.github.com>
2024-07-31 14:04:00 +0900

    pbs-70--feat: user domain

---
+++ backend/src/main/java/com/pobluesky/backend/domain/user/entity/Customer.java
@@ -0,0 +1,63 @@
+package com.pobluesky.backend.domain.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customers")
+public class Customer extends User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userNo;
+
+    private String customerCode;
+
+    private String customerName;
+
+    /*
+    Builder Pattern
+     */
+    @Builder
+    private Customer(
+        String name,
+        String email,
+        String password,
+        String phone,
+        String customerCode,
+        String customerName
+    ) {
+        // TODO : 빈칸일 경우 valudate
+//        if (name.isBlank() || email.isBlank() || password.isBlank()) {
+//            throw new CommonException(ErrorCode.INVALID_REQUEST);
+//        }
+
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.customerCode = customerCode;
+        this.customerName = customerName;
+    }
+
+    public void updateCustomer(
+        String name,
+        String email,
+        String password,
+        String phone
+    ) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+}
--- backend/src/main/java/com/pobluesky/backend/domain/user/entity/User.java
+++ backend/src/main/java/com/pobluesky/backend/domain/user/entity/User.java
@@ -1,4 +1,14 @@
 package com.pobluesky.backend.domain.user.entity;
 
-public abstract class User extends com.pobluesky.inquiry_system.global.BaseEntity {
+import com.pobluesky.backend.global.BaseEntity;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass // 상위 추상 클래스. 속성을 하위 클래스에 전달해줌
+public abstract class User extends BaseEntity {
+    protected String name;
+    protected String email;
+    protected String password;
+    protected String phone;
 }
