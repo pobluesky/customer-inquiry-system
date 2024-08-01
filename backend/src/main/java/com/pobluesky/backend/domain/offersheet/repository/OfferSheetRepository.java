37c0fd707176d98731915467cf76a0a48fa99f76
mmmtobezip <mmmszip@gmail.com>
2024-08-02 02:38:35 +0900

    pbs-75--feat: OfferSheet Repository 구현

--- backend/src/main/java/com/pobluesky/backend/domain/offersheet/repository/OfferSheetRepository.java
+++ backend/src/main/java/com/pobluesky/backend/domain/offersheet/repository/OfferSheetRepository.java
@@ -1,5 +1,12 @@
 package com.pobluesky.backend.domain.offersheet.repository;
 
-public class OfferSheetRepository {
+import com.pobluesky.backend.domain.inquiry.entity.Inquiry;
+import com.pobluesky.backend.domain.offersheet.entity.OfferSheet;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface OfferSheetRepository extends JpaRepository<OfferSheet, Long> {
+    Optional<OfferSheet> findByInquiry(Inquiry inquiry);
 }
