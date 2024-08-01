09fe3d98e737cd2a3a69d8b538924c069d8a8512
mmmtobezip <mmmszip@gmail.com>
2024-08-02 02:48:54 +0900

    pbs-75--feat: Customer Entity 구현

--- backend/src/main/java/com/pobluesky/backend/domain/user/entity/Customer.java
+++ backend/src/main/java/com/pobluesky/backend/domain/user/entity/Customer.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,8 +17,48 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Table(name = "customers")
 public class Customer extends User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long customerNo;
+    private Long customerId;
+
+    private String customerCode;
+
+    private String customerName;
+
+    /*
+    Builder Pattern
+     */
+    @Builder
+    private Customer(
+        String name,
+        String email,
+        String password,
+        String phone,
+        String customerCode,
+        String customerName
+    ) {
+        // TODO : 빈칸일 경우 validate
+//        if (name.isBlank() || email.isBlank() || password.isBlank()) {
+//            throw new CommonException(ErrorCode.INVALID_REQUEST);
+//        }
+
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.customerCode = customerCode;
+        this.customerName = customerName;
+    }
+
+    public void updateCustomer(
+        String name,
+        String email,
+        String password,
+        String phone
+    ) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
 }
