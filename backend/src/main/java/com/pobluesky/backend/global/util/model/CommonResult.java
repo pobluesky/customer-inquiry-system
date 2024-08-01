a58375405bec8d4977c4024a04092cf66a346f27
mmmtobezip <mmmszip@gmail.com>
2024-08-02 02:45:48 +0900

    pbs-75--feat: Global response 응답 구조 구현

---
+++ backend/src/main/java/com/pobluesky/backend/global/util/model/CommonResult.java
@@ -0,0 +1,11 @@
+package com.pobluesky.backend.global.util.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommonResult {
+    private String code;
+    private String message;
+}
