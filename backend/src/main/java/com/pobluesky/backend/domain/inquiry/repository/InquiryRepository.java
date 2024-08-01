568b1cc64777af3cfdd91bf8f14778ccef9916c3
Jiyoon Bae <88182667+Jiyoongrace@users.noreply.github.com>
2024-08-01 18:36:53 +0900

    cache clear

---
+++ backend/src/main/java/com/pobluesky/backend/domain/inquiry/repository/InquiryRepository.java
@@ -0,0 +1,10 @@
+package com.pobluesky.backend.domain.inquiry.repository;
+
+import com.pobluesky.backend.domain.inquiry.entity.Inquiry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
+
+}
