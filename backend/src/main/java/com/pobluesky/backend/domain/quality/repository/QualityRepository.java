0afdc5822673aee13d09a950640fcbc03218f765
Jiyoongrace <giyun3632@naver.com>
2024-08-01 11:04:05 +0900

    pbs-77--fix: Quality Repository

--- backend/src/main/java/com/pobluesky/backend/domain/quality/repository/QualityRepository.java
+++ backend/src/main/java/com/pobluesky/backend/domain/quality/repository/QualityRepository.java
@@ -1,26 +1,9 @@
 package com.pobluesky.backend.domain.quality.repository;
 
 import com.pobluesky.backend.domain.quality.entity.Quality;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class QualityRepository {
-
-    public List<Quality> findAll() {
-        return null;
-    }
-
-    public Quality save(Quality quality) {
-        return null;
-    }
-
-    public Optional<Object> findById(Long qualityNo) {
-        return null;
-    }
-
-    public void deleteById(Long qualityNo) {
-
-    }
+public interface QualityRepository extends JpaRepository<Quality, Long> {
 }
