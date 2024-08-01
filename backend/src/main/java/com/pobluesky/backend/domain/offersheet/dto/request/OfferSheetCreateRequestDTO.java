10fe5c61d944bcdee42901c8e0a93cf2fd8864f4
mmmtobezip <mmmszip@gmail.com>
2024-08-02 02:36:52 +0900

    pbs-75--feat: OfferSheet Request DTO 구현

---
+++ backend/src/main/java/com/pobluesky/backend/domain/offersheet/dto/request/OfferSheetCreateRequestDTO.java
@@ -0,0 +1,50 @@
+package com.pobluesky.backend.domain.offersheet.dto.request;
+
+import com.pobluesky.backend.domain.offersheet.entity.OfferSheet;
+import com.pobluesky.backend.domain.inquiry.entity.Inquiry;
+import com.pobluesky.backend.domain.user.entity.Customer;
+import java.time.LocalDate;
+
+public record OfferSheetCreateRequestDTO(
+    String product,
+    String specification,
+    String surfaceFinish,
+    String usage,
+    String thickness,
+    String diameter,
+    String width,
+    String quantity,
+    String price,
+    String unitMinWeight,
+    String unitMaxWeight,
+    String edge,
+    String priceTerms,
+    String paymentTerms,
+    LocalDate shipment,
+    LocalDate validity,
+    String destination
+) {
+    public OfferSheet toOfferSheetEntity(Inquiry inquiry, Customer customer) {
+        return OfferSheet.builder()
+            .inquiry(inquiry)
+            .customer(customer)
+            .product(product)
+            .specification(specification)
+            .surfaceFinish(surfaceFinish)
+            .usage(usage)
+            .thickness(thickness)
+            .diameter(diameter)
+            .width(width)
+            .quantity(quantity)
+            .price(price)
+            .unitMinWeight(unitMinWeight)
+            .unitMaxWeight(unitMaxWeight)
+            .edge(edge)
+            .priceTerms(priceTerms)
+            .paymentTerms(paymentTerms)
+            .shipment(shipment)
+            .validity(validity)
+            .destination(destination)
+            .build();
+    }
+}
