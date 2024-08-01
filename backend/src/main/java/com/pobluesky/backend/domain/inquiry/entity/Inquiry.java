20dfd365f8b3d16eae0b1022145e2f470befc9c8
mmmtobezip <mmmszip@gmail.com>
2024-08-02 02:47:57 +0900

    pbs-75--feat: Inquiry Entity 구현

--- backend/src/main/java/com/pobluesky/backend/domain/inquiry/entity/Inquiry.java
+++ backend/src/main/java/com/pobluesky/backend/domain/inquiry/entity/Inquiry.java
@@ -1,7 +1,12 @@
 package com.pobluesky.backend.domain.inquiry.entity;
 
+import com.pobluesky.backend.domain.user.entity.Customer;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,5 +20,10 @@ import lombok.NoArgsConstructor;
 public class Inquiry {
 
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inquiryId;
+
+    @OneToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
 }
