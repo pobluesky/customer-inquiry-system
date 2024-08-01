ebc69e7d514970706225bb1ab75e3d3439f6a9f5
mmmtobezip <mmmszip@gmail.com>
2024-08-02 02:37:26 +0900

    pbs-75--feat: OfferSheet Controller 구현

--- backend/src/main/java/com/pobluesky/backend/domain/offersheet/controller/OfferSheetController.java
+++ backend/src/main/java/com/pobluesky/backend/domain/offersheet/controller/OfferSheetController.java
@@ -1,5 +1,55 @@
 package com.pobluesky.backend.domain.offersheet.controller;
 
-public class OfferSheetController {
+import com.pobluesky.backend.domain.offersheet.dto.request.OfferSheetCreateRequestDTO;
+import com.pobluesky.backend.domain.offersheet.dto.request.OfferSheetUpdateRequestDTO;
+import com.pobluesky.backend.domain.offersheet.dto.response.OfferSheetResponseDTO;
+import com.pobluesky.backend.domain.offersheet.service.OfferSheetService;
+import com.pobluesky.backend.global.util.ResponseFactory;
+import com.pobluesky.backend.global.util.model.JsonResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/offersheet")
+public class OfferSheetController {
+    private final OfferSheetService offerSheetService;
+
+    @PostMapping("/{inquiryId}")
+    public ResponseEntity<JsonResult> createOfferSheet(
+        @PathVariable Long inquiryId,
+        @RequestBody OfferSheetCreateRequestDTO osCreateRequestDTO) {
+        OfferSheetResponseDTO response = offerSheetService.createOfferSheet(inquiryId, osCreateRequestDTO);
+        return ResponseEntity.status(HttpStatus.OK)
+
+            .body(ResponseFactory.getSuccessJsonResult(response));
+    }
+
+    @GetMapping("/{inquiryId}")
+    public ResponseEntity<JsonResult> getOfferSheetByInquiryId(@PathVariable Long inquiryId) {
+        OfferSheetResponseDTO response = offerSheetService.getOfferSheetByInquiryId(inquiryId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseFactory.getSuccessJsonResult(response));
+    }
+
+    @PutMapping("/{inquiryId}")
+    public ResponseEntity<JsonResult> updateOfferSheetByInquiryId(
+        @PathVariable Long inquiryId,
+        @RequestBody OfferSheetUpdateRequestDTO offerSheetUpdateRequestDTO) {
+
+        OfferSheetResponseDTO response = offerSheetService.updateOfferSheetByInquiryId(inquiryId, offerSheetUpdateRequestDTO);
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(ResponseFactory.getSuccessJsonResult(response));
+    }
 }
