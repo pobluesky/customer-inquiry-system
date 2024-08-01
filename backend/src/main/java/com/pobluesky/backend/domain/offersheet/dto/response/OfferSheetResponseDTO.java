5911a6443548e314f07d87c183a9724152e48a6b
mmmtobezip <mmmszip@gmail.com>
2024-08-02 02:36:04 +0900

    pbs-75--feat: OfferSheet Response DTO 구현

---
+++ backend/src/main/java/com/pobluesky/backend/domain/offersheet/dto/response/OfferSheetResponseDTO.java
@@ -0,0 +1,56 @@
+package com.pobluesky.backend.domain.offersheet.dto.response;
+
+import com.pobluesky.backend.domain.offersheet.entity.OfferSheet;
+import lombok.Builder;
+import java.time.LocalDate;
+
+@Builder
+public record OfferSheetResponseDTO(
+    Long offerSheetId,
+    Long inquiryId,
+    Long customerId,
+//    String customerName,
+    String product,
+    String specification,
+    String surfaceFinish,
+    String usage,
+    String thickness,
+    String diameter,
+    String width,
+    String quantity,
+    String price,
+    String unitMinWeight,
+    String unitMaxWeight,
+    String edge,
+    String priceTerms,
+    String paymentTerms,
+    LocalDate shipment,
+    LocalDate validity,
+    String destination
+) {
+    public static OfferSheetResponseDTO from(OfferSheet offerSheet) {
+        return OfferSheetResponseDTO.builder()
+            .offerSheetId(offerSheet.getOfferSheetId())
+            .inquiryId(offerSheet.getInquiry().getInquiryId())
+            .customerId(offerSheet.getCustomer().getCustomerId())
+            //.customerName(offerSheet.getCustomer().getCustomerName())
+            .product(offerSheet.getProduct())
+            .specification(offerSheet.getSpecification())
+            .surfaceFinish(offerSheet.getSurfaceFinish())
+            .usage(offerSheet.getUsage())
+            .thickness(offerSheet.getThickness())
+            .diameter(offerSheet.getDiameter())
+            .width(offerSheet.getWidth())
+            .quantity(offerSheet.getQuantity())
+            .price(offerSheet.getPrice())
+            .unitMinWeight(offerSheet.getUnitMinWeight())
+            .unitMaxWeight(offerSheet.getUnitMaxWeight())
+            .edge(offerSheet.getEdge())
+            .priceTerms(offerSheet.getPriceTerms())
+            .paymentTerms(offerSheet.getPaymentTerms())
+            .shipment(offerSheet.getShipment())
+            .validity(offerSheet.getValidity())
+            .destination(offerSheet.getDestination())
+            .build();
+    }
+}
