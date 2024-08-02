0038f13ad4f09e7a7df76aea0c03549bae22042a
heejinssss <heejin.dev33@gmail.com>
2024-08-02 21:14:53 +0900

    pbs-85--chore: 패키지 삭제 및 재설치

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -1,42 +1,38 @@
 const path = require('path');
 
-module.exports = function (env) {
+module.exports = function(env) {
     return {
-        mode: 'none',
+        mode: "none",
         entry: path.resolve(`src/index.js`),
         output: {
-            path: path.resolve('../backend/src/main/resources'),
+            path: path.resolve('public'),
             filename: 'assets/js/main.js',
-            assetModuleFilename: 'assets/images/[hash][ext]',
+            assetModuleFilename: 'assets/images/[hash][ext]'
         },
         module: {
-            rules: [
-                {
-                    test: /\.js/i,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader',
-                    options: {
-                        configFile: path.resolve('config/babel.config.json'),
-                    },
-                },
-                {
-                    test: /\.(c|sa|sc)ss$/i,
-                    use: [
-                        'style-loader',
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: true,
-                            },
-                        },
-                        'sass-loader',
-                    ],
-                },
-                {
-                    test: /\.(png|gif|jp?eg|svg|ico|tif?f|bmp)/i,
-                    type: 'asset/resource',
-                },
-            ],
+            rules:[{
+                test: /\.js/i,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    configFile: path.resolve('config/babel.config.json')
+                }
+            }, {
+                test: /\.(c|sa|sc)ss$/i,
+                use:[
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }, 
+                    'sass-loader'
+                ]
+            }, {
+                test: /\.(png|gif|jp?eg|svg|ico|tif?f|bmp)/i,
+                type: 'asset/resource'
+            }]
         },
         devServer: {
             host: '0.0.0.0',
@@ -44,6 +40,7 @@ module.exports = function (env) {
             liveReload: true,
             compress: true,
             hot: false,
-        },
+            historyApiFallback: true,
+        }    
     };
-};
+}
