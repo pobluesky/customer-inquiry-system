995751b39436fd87cd8303ca890081c0f5bfea53
heejinssss <heejin.dev33@gmail.com>
2024-08-04 11:23:07 +0900

    pbs-85--feat: OfferTable 수정

--- frontend/src/pages/inq/Inq.js
+++ frontend/src/pages/inq/Inq.js
@@ -5,7 +5,8 @@ import TextEditor from '../../components/mocules/TextEditor';
 import RequestBar from '../../components/mocules/RequestBar';
 import Toggle from '../../components/atoms/Toggle';
 import Category from '../../components/atoms/Category';
-import OfferTable from '../../components/mocules/OfferTable';
+import OfferTable from '../../components/organisms/OfferTable';
+import DateInput from '../../components/mocules/DateInput';
 
 function Inq() {
     const [originalText, setOriginalText] = useState('');
@@ -25,17 +26,21 @@ function Inq() {
                     </div>
 
                     {isChecked ? (
-                        <>
+                        <div style={{ backgroundColor: '#f0f8fc', paddingTop: '2vh' }}>
                             <TextEditor originalText={originalText} setOriginalText={setOriginalText} />
-                            <div><Category categoryName={'1. 고객사'} />Posco Asia</div>
+                            <div>
+                                <Category categoryName={'1. 고객사'} />
+                                Posco Asia
+                            </div>
                             <Category categoryName={'2. Offer-Sheet'} />
                             <OfferTable />
                             <Category categoryName={'3. Price Term'} />
                             <Category categoryName={'4. Shipment'} />
+                            <DateInput />
                             <Category categoryName={'5. Payment Term'} />
                             <Category categoryName={'6. Destination'} />
                             <Category categoryName={'7. Validity'} />
-                        </>
+                        </div>
                     ) : (
                         ''
                     )}
