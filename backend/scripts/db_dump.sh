5d944a90076e879be2bcd68b9fb4974437c6ac04
mmmtobezip <mmmszip@gmail.com>
2024-08-05 19:42:38 +0900

    pbs-83--remove: docker 파일 & db script 삭제

--- backend/scripts/db_dump.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-current_timestamp=$(date +%s)
-dump_command="pg_dump -c -U postgres -d inquirysystem"
-container_id=$(docker ps | grep postgres | awk '{print $1}')
-docker exec -t ${container_id} $dump_command > db_dump_files/db_dump_${current_timestamp}.sql
