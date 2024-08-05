d7e8e0c960ada9f28b7d41f76d642a33a39bb29c
Jiyoon Bae <88182667+Jiyoongrace@users.noreply.github.com>
2024-08-05 17:42:13 +0900

    pbs-103--feat: Intro.js 추가

--- frontend/src/pages/intro/Intro.js
+++ frontend/src/pages/intro/Intro.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { FirstIntro, SecondIntro, ThirdIntro, FourthIntro } from './section';
 
 function Intro() {
-    return (
-        <>
-            <div>
-                <Link to="/login">로그인</Link>
-            </div>
-            <div>
-                <Link to="/join">회원가입</Link>
-            </div>
-            <div>
-                <Link to="/inq">Inquiry</Link>
-            </div>
-            <div>
-                <Link to="/voc">VoC</Link>
-            </div>
-            <div>
-                <Link to="/dashboard">DashBoard</Link>
-            </div>
-        </>
-    );
+
+  return (
+      <>{/*
+        <div>
+          <Link to="/login">로그인</Link>
+        </div>
+        <div>
+          <Link to="/join">회원가입</Link>
+        </div>
+        <div>
+          <Link to="/inq">Inquiry</Link>
+        </div>
+        <div>
+          <Link to="/voc">VoC</Link>
+        </div>
+        <div>
+          <Link to="/dashboard">DashBoard</Link>
+        </div>
+        */}
+        <FirstIntro/>
+        <SecondIntro/>
+        <ThirdIntro/>
+        <FourthIntro/>
+      </>
+  );
 }
 
 export default Intro;
