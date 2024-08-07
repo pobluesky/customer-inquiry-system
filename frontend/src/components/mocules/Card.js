a9df1be725674ca3a95c9aa2b133955ac01d9e86
heejinssss <heejin.dev33@gmail.com>
2024-08-07 15:05:54 +0900

    pbs-129--feat: Question Card Component

--- frontend/src/components/mocules/Card.js
+++ frontend/src/components/mocules/Card.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import Button from './../atoms/Button';
+import arrow from '../../assets/css/icons/voc/arrow.svg';
+import { Card_Container, Card_Text, Card_Title, Card_Move_To, Answer } from '../../assets/css/Voc.css';
 
 function Card() {
-    const inquiryId = 'INQ20240711';
     const createdAt = '2024-08-07';
-    // 답변 등록일 변수 추가 필요
+    const updatedAt = ''; // 답변 등록일
     const title = '자동차 규격 재검토 문의드립니다.';
 
     return (
-        <div style={{ width: '360px', height: '360px', border: 'solid 1px #00000', borderRadius: '12px', backgroundColor: '#ffffff', boxShadow: 'rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px', display: 'grid' }}>
-            <div style={{ display: 'grid' }}>
-                <Button btnName={'답변 대기'} onClick={console.log('Click')} width={'132px'} height={'40px'} backgroundColor={'#007aff'} textColor={'#ffffff'} fontSize={'20px'} border={'none'} borderRadius={'12px'} cursor={'pointer'} />
+        <div className={Card_Container}>
+            <Button btnName={'답변 대기'} onClick={console.log('Click')} width={'132px'} height={'40px'} margin={'32px 0 0 32px'} backgroundColor={'#007aff'} textColor={'#ffffff'} fontSize={'20px'} border={'none'} borderRadius={'16px'} cursor={'pointer'} />
+            <div className={Card_Text}>문의 등록일&nbsp;&nbsp;&nbsp;{createdAt}</div>
+            <div className={Card_Text}>답변 등록일&nbsp;{updatedAt}</div>
+            <div className={Card_Title}>{title}</div>
+            <div className={Card_Move_To}>
+                <div className={Answer}>답변 보기</div>
+                <div><img src={arrow} /></div>
             </div>
-            <div>#{inquiryId}</div>
-            <div>문의 등록일 {createdAt}</div>
-            <div>답변 등록일 </div>
-            <div>{title}</div>
         </div>
     );
 }
