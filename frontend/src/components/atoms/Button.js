982385f5a027e96f99dd1535e8a3dc4fe548711c
heejinssss <heejin.dev33@gmail.com>
2024-08-07 20:07:07 +0900

    pbs-129--feat: Button & DateInput props 수정

--- frontend/src/components/atoms/Button.js
+++ frontend/src/components/atoms/Button.js
@@ -1,16 +1,14 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 
-function Button({ onClick, btnName, width, height, margin, backgroundColor, textColor, border, borderRadius, display, fontSize }) {
+function Button({ onClick, btnName, width, height, margin, backgroundColor, textColor, border, borderRadius, fontSize }) {
   return (
-      <span>
             <button
                 onClick={onClick}
                 style={{ width: `${width}`, height: `${height}`, margin: `${margin}`, backgroundColor: `${backgroundColor}`, color: `${textColor}`, fontSize: `${fontSize}`, border: `${border}`, borderRadius: `${borderRadius}`, cursor: 'pointer'}}
             >
             {btnName}
             </button>
-        </span>
   );
 }
 
