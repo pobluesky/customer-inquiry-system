8c188ee7b3a892f0a7b66153565e13fd5777852a
heejinssss <heejin.dev33@gmail.com>
2024-08-07 13:31:28 +0900

    pbs-114--feat: Button props 수정

--- frontend/src/components/atoms/Button.js
+++ frontend/src/components/atoms/Button.js
@@ -1,23 +1,14 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 
-function Button({ onClick, btnName, width, height, margin, marginRight, marginLeft, backgroundColor, textColor, border, borderRadius, fontSize }) {
+function Button({ onClick, btnName, width, height, margin, backgroundColor, textColor, border, borderRadius, display, fontSize }) {
   return (
       <span>
             <button
                 onClick={onClick}
-                style={{ width: `${width}`,
-                  height: `${height}`,
-                  margin: `${margin}`,
-                  marginRight: `${marginRight}`,
-                  marginLeft: `${marginLeft}`,
-                  backgroundColor: `${backgroundColor}`,
-                  color: `${textColor}`,
-                  fontSize: `${fontSize}`,
-                  border: `${border}`,
-                  borderRadius: `${borderRadius}`,
-                  cursor: 'pointer'}}>
-                {btnName}
+                style={{ width: `${width}`, height: `${height}`, margin: `${margin}`, backgroundColor: `${backgroundColor}`, color: `${textColor}`, fontSize: `${fontSize}`, border: `${border}`, borderRadius: `${borderRadius}`, cursor: 'pointer'}}
+            >
+            {btnName}
             </button>
         </span>
   );
