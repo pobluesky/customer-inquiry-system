3b4dfcd069780df94ddadd848608831b357278db
born-A <93516595+born-A@users.noreply.github.com>
2024-08-07 15:01:42 +0900

    pbs-108--feat: security - repo

--- backend/src/main/java/com/pobluesky/backend/domain/user/repository/CustomerRepository.java
+++ backend/src/main/java/com/pobluesky/backend/domain/user/repository/CustomerRepository.java
@@ -1,9 +1,13 @@
 package com.pobluesky.backend.domain.user.repository;
 
 import com.pobluesky.backend.domain.user.entity.Customer;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
--- backend/src/main/java/com/pobluesky/backend/domain/user/repository/ManagerRepository.java
+++ backend/src/main/java/com/pobluesky/backend/domain/user/repository/ManagerRepository.java
@@ -1,7 +1,13 @@
 package com.pobluesky.backend.domain.user.repository;
 
+import com.pobluesky.backend.domain.user.entity.Customer;
 import com.pobluesky.backend.domain.user.entity.Manager;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
+
+    Optional<Manager> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
