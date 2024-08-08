380a92d6d84ec5fdcd97074d8fc6853193d58bf0
parlyresk <parlyresk325@gmail.com>
2024-08-08 18:53:33 +0900

    pbs-113--chore: controller api add address

--- backend/src/main/java/com/pobluesky/backend/domain/file/controller/FileUploadController.java
+++ backend/src/main/java/com/pobluesky/backend/domain/file/controller/FileUploadController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api")
 public class FileUploadController {
 
     @Autowired
