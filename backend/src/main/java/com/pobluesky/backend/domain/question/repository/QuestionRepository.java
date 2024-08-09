5ba06deac8913c963afffbac850a8aeecf5a865d
heejinssss <heejin.dev33@gmail.com>
2024-08-09 16:52:00 +0900

    pbs-139--refactor: repository 주석 제거

--- backend/src/main/java/com/pobluesky/backend/domain/question/repository/QuestionRepository.java
+++ backend/src/main/java/com/pobluesky/backend/domain/question/repository/QuestionRepository.java
@@ -5,10 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-//    Optional<Question> findByQuestionId(Long questionId);
     List<Question> findByCustomer_CustomerId(Long customerId);
 }
