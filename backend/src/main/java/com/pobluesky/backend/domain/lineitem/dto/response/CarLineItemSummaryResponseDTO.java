e817de5e27f9bc0da6a2a670f1703bffa8fd71f2
mmmtobezip <mmmszip@gmail.com>
2024-08-12 03:30:17 +0900

    pbs-119--refactor: Sort&Paging Response 수정

--- backend/src/main/java/com/pobluesky/backend/domain/lineitem/dto/response/CarLineItemSummaryResponseDTO.java
+++ backend/src/main/java/com/pobluesky/backend/domain/lineitem/dto/response/CarLineItemSummaryResponseDTO.java
@@ -33,7 +33,7 @@ public class CarLineItemSummaryResponseDTO extends LineItemResponseDTO {
     public static CarLineItemSummaryResponseDTO of(CarLineItem carLineItem) {
 
         InquirySummaryResponseDTO inquiryDTO =
-            InquirySummaryResponseDTO.from(carLineItem.getInquiry());
+            InquirySummaryResponseDTO.from(carLineItem.getInquiry(), carLineItem);
 
         return CarLineItemSummaryResponseDTO.builder()
             .lineItemId(carLineItem.getLineItemId())
