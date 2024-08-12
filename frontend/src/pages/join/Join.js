311d49bce01607331a035b179761555579351b1a
heejinssss <heejin.dev33@gmail.com>
2024-08-12 20:07:56 +0900

    pbs-142--feat: 로그인/회원가입 컴포넌트 수정

--- frontend/src/pages/join/Join.js
+++ frontend/src/pages/join/Join.js
@@ -1,8 +1,8 @@
 import React, { useRef, useState } from 'react';
 import Button from '../../components/atoms/Button';
 import Header from '../../components/mocules/Header';
-import Input from '../../components/atoms/JoinInput';
-import { Container_Join, Join_Title, Join_Name_No } from '../../assets/css/Member.css';
+import Input from '../../components/atoms/Input';
+import { SignUp } from '../../assets/css/Auth.css';
 
 function Join() {
     const nameRef = useRef(null);
@@ -40,50 +40,173 @@ function Join() {
 
     return (
         <div>
-            <Header user={false} login={false} inq={true} voc={true} dashboard={true} />
-            <div className={Container_Join}>
-                {!check ? (
-                    <>
-                        <div className={Join_Title}>회원가입</div>
-                        {/* 이름 & 사번 입력 창 */}
-                        <div style={{ marginTop: '8vh' }} />
-                        <Input category={'이름'} placeholder={'김숙하'} onChange={nameInput} ref={nameRef} />
-                        <Input category={'사번'} placeholder={'123456789'} onChange={noInput} ref={noRef} />
+            <Header
+                user={false}
+                login={false}
+                inq={true}
+                voc={true}
+                dashboard={true}
+            />
+            <div className={SignUp}>
+                <div>
+                    {!check ? (
+                        <>
+                            <div>회원가입</div>
+                            {/* 이름 & 사번 입력 창 */}
+                            <div>
+                                <Input
+                                    width={'336px'}
+                                    height={'48px'}
+                                    margin={'0 0 24px 0'}
+                                    padding={'0 0 0 20px'}
+                                    border={'solid 1px #c1c1c1'}
+                                    borderRadius={'12px'}
+                                    fontSize={'20px'}
+                                    needCategory={true}
+                                    categoryName={'이름'}
+                                    categoryWidth={'360px'}
+                                    categoryColor={'#03507d'}
+                                    CategoryFontWeight={'600'}
+                                    categoryMargin={'0 auto 12px auto'}
+                                    categoryTextAlign={'left'}
+                                    placeholder={'김숙하'}
+                                    onChange={nameInput}
+                                    ref={nameRef}
+                                />
+                                <Input
+                                    width={'336px'}
+                                    height={'48px'}
+                                    padding={'0 0 0 20px'}
+                                    border={'solid 1px #c1c1c1'}
+                                    borderRadius={'12px'}
+                                    fontSize={'20px'}
+                                    needCategory={true}
+                                    categoryName={'사번'}
+                                    categoryWidth={'360px'}
+                                    categoryColor={'#03507d'}
+                                    CategoryFontWeight={'600'}
+                                    categoryMargin={'0 auto 12px auto'}
+                                    categoryTextAlign={'left'}
+                                    placeholder={'123456789'}
+                                    onChange={noInput}
+                                    ref={noRef}
+                                />
+                            </div>
 
-                        {/* 역할 조회 버튼 */}
-                        <div style={{ marginTop: '4vh' }}>
-                            <Button
-                                onClick={() => {
-                                    findRole();
-                                    inputClear();
-                                    setCheck(true);
-                                }}
-                                btnName={'역할 조회'}
-                                width={'360px'}
-                                height={'44px'}
-                                margin={'12px'}
-                                backgroundColor={'#03507D'}
-                                textColor={'#EEEEEE'}
-                                borderRadius={'12px'}
-                                fontSize={'20px'}
-                            />
-                        </div>
-                    </>
-                ) : (
-                    <>
-                        <div className={Join_Name_No} style={{ marginTop: '8vh' }} />
-                        {/* 이름 & 사번 입력 창 */}
-                        <Input category={'권한'} value={`${role}`}></Input>
-                        <Input category={'이메일'} placeholder={'poscodx@posco.co.kr'} onChange={emailInput} value={email} />
-                        <Input category={'비밀번호'} placeholder={'********'} onChange={passwordInput} value={password} />
-                        <Input category={'비밀번호 확인'} placeholder={'********'} onChange={passwordInput} value={password} />
+                            {/* 역할 조회 버튼 */}
+                            <div>
+                                <Button
+                                    onClick={() => {
+                                        findRole();
+                                        inputClear();
+                                        setCheck(true);
+                                    }}
+                                    btnName={'역할 조회'}
+                                    width={'360px'}
+                                    height={'44px'}
+                                    backgroundColor={'#03507D'}
+                                    textColor={'#EEEEEE'}
+                                    borderRadius={'12px'}
+                                    fontSize={'20px'}
+                                />
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div />
+                            {/* 이름 & 사번 입력 창 */}
+                            <div>
+                                <Input
+                                    width={'336px'}
+                                    height={'48px'}
+                                    padding={'0 0 0 20px'}
+                                    border={'solid 1px #c1c1c1'}
+                                    borderRadius={'12px'}
+                                    fontSize={'20px'}
+                                    needCategory={true}
+                                    categoryName={'권한'}
+                                    categoryWidth={'360px'}
+                                    categoryColor={'#03507d'}
+                                    CategoryFontWeight={'600'}
+                                    categoryMargin={'0 auto 12px auto'}
+                                    categoryTextAlign={'left'}
+                                    value={`${role}`}
+                                />
+                                <Input
+                                    width={'336px'}
+                                    height={'48px'}
+                                    margin={'0 0 24px 0'}
+                                    padding={'0 0 0 20px'}
+                                    border={'solid 1px #c1c1c1'}
+                                    borderRadius={'12px'}
+                                    fontSize={'20px'}
+                                    needCategory={true}
+                                    categoryName={'이메일'}
+                                    categoryWidth={'360px'}
+                                    categoryColor={'#03507d'}
+                                    CategoryFontWeight={'600'}
+                                    categoryMargin={'0 auto 12px auto'}
+                                    categoryTextAlign={'left'}
+                                    placeholder={'poscodx@posco.co.kr'}
+                                    onChange={emailInput}
+                                    value={email}
+                                />
+                                <Input
+                                    width={'336px'}
+                                    height={'48px'}
+                                    margin={'0 0 24px 0'}
+                                    padding={'0 0 0 20px'}
+                                    border={'solid 1px #c1c1c1'}
+                                    borderRadius={'12px'}
+                                    fontSize={'20px'}
+                                    needCategory={true}
+                                    categoryName={'비밀번호'}
+                                    categoryWidth={'360px'}
+                                    categoryColor={'#03507d'}
+                                    CategoryFontWeight={'600'}
+                                    categoryMargin={'0 auto 12px auto'}
+                                    categoryTextAlign={'left'}
+                                    placeholder={'********'}
+                                    onChange={passwordInput}
+                                    value={password}
+                                />
+                                <Input
+                                    width={'336px'}
+                                    height={'48px'}
+                                    margin={'0 0 0 0'}
+                                    padding={'0 0 0 20px'}
+                                    border={'solid 1px #c1c1c1'}
+                                    borderRadius={'12px'}
+                                    fontSize={'20px'}
+                                    needCategory={true}
+                                    categoryName={'비밀번호 확인'}
+                                    categoryWidth={'360px'}
+                                    categoryColor={'#03507d'}
+                                    CategoryFontWeight={'600'}
+                                    categoryMargin={'0 auto 12px auto'}
+                                    categoryTextAlign={'left'}
+                                    placeholder={'********'}
+                                    onChange={passwordInput}
+                                    value={password}
+                                />
+                            </div>
 
-                        {/* 회원가입 버튼 */}
-                        <div style={{ marginTop: '4vh' }}>
-                            <Button btnName={'회원가입'} width={'360px'} height={'44px'} margin={'12px'} backgroundColor={'#03507D'} textColor={'#EEEEEE'} fontSize={'20px'} border={'solid #c1c1c1 1px'} borderRadius={'12px'} />
-                        </div>
-                    </>
-                )}
+                            {/* 회원가입 버튼 */}
+                            <div>
+                                <Button
+                                    btnName={'회원가입'}
+                                    width={'360px'}
+                                    height={'44px'}
+                                    backgroundColor={'#03507D'}
+                                    textColor={'#EEEEEE'}
+                                    fontSize={'20px'}
+                                    border={'solid #c1c1c1 1px'}
+                                    borderRadius={'12px'}
+                                />
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
         </div>
     );
--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,34 +1,102 @@
 import React from 'react';
 import Button from '../../components/atoms/Button';
 import Header from '../../components/mocules/Header';
-import Input from '../../components/atoms/JoinInput';
-import { Container_Login, Login_Title, Login_Email_Password, Join_Line, Join_Link } from '../../assets/css/Member.css';
+import Input from '../../components/atoms/Input';
+import { SignIn } from '../../assets/css/Auth.css';
 
 function Login() {
     return (
         <div>
-            <Header user={true} login={false} inq={true} voc={true} dashboard={true} />
-            <div className={Container_Login}>
-                <div className={Login_Title}>로그인</div>
-                <div className={Login_Email_Password}>이메일과 비밀번호를 입력해주세요.</div>
+            <Header
+                user={true}
+                login={false}
+                inq={true}
+                voc={true}
+                dashboard={true}
+            />
+            <div className={SignIn}>
+                <div>
+                    <div>로그인</div>
+                    <div>이메일과 비밀번호를 입력해주세요.</div>
 
-                {/* 이메일 & 비밀번호 입력 창 */}
-                <Input category={'이메일'} placeholder={'poscodx@posco.co.kr'} />
-                <Input category={'비밀번호'} placeholder={'********'} />
+                    {/* 이메일 & 비밀번호 입력 창 */}
+                    <div>
+                        <Input
+                            width={'336px'}
+                            height={'48px'}
+                            margin={'0 0 24px 0'}
+                            padding={'0 0 0 20px'}
+                            border={'solid 1px #c1c1c1'}
+                            borderRadius={'12px'}
+                            fontSize={'20px'}
+                            needCategory={true}
+                            categoryName={'이메일'}
+                            categoryWidth={'360px'}
+                            categoryColor={'#03507d'}
+                            CategoryFontWeight={'600'}
+                            categoryMargin={'0 auto 12px auto'}
+                            categoryTextAlign={'left'}
+                            placeholder={'poscodx@posco.co.kr'}
+                            // onChange={onChange}
+                            // ref={ref}
+                        />
+                    </div>
+                    <div>
+                        <Input
+                            width={'336px'}
+                            height={'48px'}
+                            padding={'0 0 0 20px'}
+                            border={'solid 1px #c1c1c1'}
+                            borderRadius={'12px'}
+                            fontSize={'20px'}
+                            needCategory={true}
+                            categoryName={'비밀번호'}
+                            categoryWidth={'360px'}
+                            categoryColor={'#03507d'}
+                            CategoryFontWeight={'600'}
+                            categoryMargin={'0 auto 12px auto'}
+                            categoryTextAlign={'left'}
+                            placeholder={'********'}
+                            // onChange={onChange}
+                            // ref={ref}
+                        />
+                    </div>
 
-                {/* 로그인 완료 & 비밀번호 찾기 버튼 */}
-                <div style={{ marginTop: '4vh' }}>
-                    <Button btnName={'로그인'} width={'168px'} height={'44px'} margin={'12px'} backgroundColor={'#03507D'} textColor={'#EEEEEE'} fontSize={'20px'} border={'solid #c1c1c1 1px'} borderRadius={'12px'} />
-                    <Button btnName={'비밀번호 찾기'} width={'168px'} height={'44px'} margin={'12px'} backgroundColor={'#EEEEEE'} textColor={'#03507D'} fontSize={'20px'} border={'solid #c1c1c1 1px'} borderRadius={'12px'} />
+                    {/* 로그인 완료 & 비밀번호 찾기 버튼 */}
+                    <div style={{ marginTop: '4vh' }}>
+                        <div>
+                            <Button
+                                btnName={'로그인'}
+                                width={'168px'}
+                                height={'44px'}
+                                margin={'12px'}
+                                backgroundColor={'#03507D'}
+                                textColor={'#EEEEEE'}
+                                fontSize={'20px'}
+                                border={'solid #c1c1c1 1px'}
+                                borderRadius={'12px'}
+                            />
+                        </div>
+                        <div>
+                            <Button
+                                btnName={'비밀번호 찾기'}
+                                width={'168px'}
+                                height={'44px'}
+                                margin={'12px'}
+                                backgroundColor={'#EEEEEE'}
+                                textColor={'#03507D'}
+                                fontSize={'20px'}
+                                border={'solid #c1c1c1 1px'}
+                                borderRadius={'12px'}
+                            />
+                        </div>
+                    </div>
+
+                    {/* 회원가입 링크 */}
+                    <div>
+                        <a href="/join">회원이 아니신가요?</a>
+                    </div>
                 </div>
-
-                {/* 회원가입 링크 */}
-                <br />
-                <hr className={Join_Line} />
-                <br />
-                <a href="/join" className={Join_Link}>
-                    회원이 아니신가요?
-                </a>
             </div>
         </div>
     );
