4b572d36b9b8957e0b39015d42a4fd83f7952904
Jiyoon Bae <88182667+Jiyoongrace@users.noreply.github.com>
2024-08-13 19:07:06 +0900

    pbs-160--feat: api token 처리 및 Cookie 관리 + 로그인/회원가입 Test 완료

---
+++ frontend/src/apis/api/inquiry.js
@@ -0,0 +1,22 @@
+import axiosInstance from "../utils/axiosInstance";
+import { getRoleFromCookie } from '../utils/tokenUtils';
+import {getCookie} from "../utils/cookies";
+
+export const getInquiry = async (customerId) => {
+  try {
+    console.log(getCookie('accessToken'));
+    const userRole = getRoleFromCookie();
+
+    if (userRole !== 'USER') { // 백엔드 수정 후 USER -> CUSTOMER, QUALITY, SALES 변경 필요
+      throw new Error('Access denied: [Role] 오류');
+    }
+
+    const response = await axiosInstance.get(`/customers/inquiries/${customerId}`);
+    // console.log("[getInquiry]: ", response.data);
+    return response.data;
+
+  } catch (error) {
+    console.error('Error fetching Inquiry:', error);
+    throw error;
+  }
+};
---
+++ frontend/src/apis/utils/axiosInstance.js
@@ -0,0 +1,100 @@
+import axios from 'axios';
+import BASE_URL from '../config/constants';
+import { getCookie, removeCookie, setCookie } from './cookies';
+import { getRoleFromCookie } from './tokenUtils';
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+      const accessToken = getCookie('accessToken');
+      if (accessToken) {
+        config.headers.Authorization = `${accessToken}`;
+      }
+      // console.log("[axiosInstance] interceptors request: ", config.headers);
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    // 응답이 성공적일 때
+    (response) => {
+      if (response && response.data) {
+        const accessToken = getCookie('accessToken');
+
+        if (accessToken) {
+          const role = getRoleFromCookie();
+          // console.log("[axiosInstance] interceptor response: ", { ...response, role });
+          return { ...response, role };
+        }
+      }
+      return response.data;
+    },
+
+    // 토큰이 유효하지 않을 경우
+    async (error) => {
+      const statusCode = error.response?.status;
+      if (statusCode === 401 || statusCode === 403) {
+        try {
+          const refreshToken = getCookie('refreshToken');
+          if (!refreshToken) {
+            removeCookie('accessToken');
+            return Promise.reject(error);
+          }
+
+          const userRole = getRoleFromCookie();
+
+          const refreshUrl = userRole === 'CUSTOMER'
+              ? '/customers/sign-in'
+              : '/managers/sign-in';
+
+          const refreshResponse = await axios.post(
+              refreshUrl,
+              null,
+              {
+                params: {
+                  accountId: '1', // 필요에 따라 조정
+                  token: refreshToken,
+                },
+                headers: {
+                  Authorization: `Bearer ${refreshToken}`,
+                },
+              }
+          );
+
+          const newAccessToken = refreshResponse.data.result.accessToken;
+          setCookie('accessToken', newAccessToken, {
+            path: '/',
+          });
+          setCookie('refreshToken', refreshResponse.data.result.refreshToken, {
+            path: '/',
+          });
+
+          error.config.headers.Authorization = `${newAccessToken}`;
+
+          const newRole = getRoleFromCookie(newAccessToken);
+
+          const updatedResponse = await axios(error.config);
+
+          return { ...updatedResponse, userRole: newRole };
+        } catch (err) {
+          removeCookie('accessToken');
+          removeCookie('refreshToken');
+          return Promise.reject(err);
+        }
+      }
+      return Promise.reject(error);
+    }
+);
+
+
+export default axiosInstance;
