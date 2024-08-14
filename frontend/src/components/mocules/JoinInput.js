915b7462fdc924e8e9642b06da83bc9b9fcc6f44
heejinssss <heejin.dev33@gmail.com>
2024-08-14 15:03:47 +0900

    pbs-129--feat: 회원가입 API 연동 및 로직 수정

---
+++ frontend/src/components/mocules/JoinInput.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Input from '../atoms/Input';
+
+const JoinInput = ({
+    margin,
+    ref,
+    value,
+    onChange,
+    type,
+    placeholder,
+    categoryName,
+    needCategory = true,
+    warningMsg,
+    needWarningMsg = true,
+}) => (
+    <Input
+        ref={ref}
+        value={value}
+        onChange={onChange}
+        type={type}
+        placeholder={placeholder}
+        width={'336px'}
+        height={'48px'}
+        margin={margin}
+        padding={'0 0 0 20px'}
+        border={'solid 1px #c1c1c1'}
+        borderRadius={'12px'}
+        fontSize={'16px'}
+        needCategory={needCategory}
+        categoryName={categoryName}
+        categoryWidth={'360px'}
+        categoryColor={'#03507d'}
+        CategoryFontWeight={'600'}
+        categoryMargin={'0 auto 12px auto'}
+        categoryTextAlign={'left'}
+        needWarningMsg={needWarningMsg}
+        warningMsg={warningMsg}
+    />
+);
+
+export default JoinInput;
