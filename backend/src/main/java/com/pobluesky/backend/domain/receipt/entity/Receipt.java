c8259a84391dbd10811b148d3845e41a702f67a4
born-A <93516595+born-A@users.noreply.github.com>
2024-08-14 18:01:58 +0900

    pbs-170--fear: receipt entity, dto

---
+++ backend/src/main/java/com/pobluesky/backend/domain/receipt/entity/Receipt.java
@@ -0,0 +1,120 @@
+package com.pobluesky.backend.domain.receipt.entity;
+
+import com.pobluesky.backend.domain.inquiry.entity.Inquiry;
+import com.pobluesky.backend.domain.offersheet.entity.OfferSheet;
+import com.pobluesky.backend.global.BaseEntity;
+import jakarta.persistence.Embeddable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Receipt extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long receiptId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "offersheet_id")
+    private OfferSheet offerSheet;
+
+    private String product;
+
+    private String specification;
+
+    private String surfaceFinish;
+
+    private String usage;
+
+    private String thickness;
+
+    private String diameter;
+
+    private String width;
+
+    private String quantity;
+
+    private String price;
+
+    private String unitMinWeight;
+
+    private String unitMaxWeight;
+
+    private String edge;
+
+    protected Boolean isActivated;
+
+    @Builder
+    public Receipt(
+        OfferSheet offerSheet,
+        String product,
+        String specification,
+        String surfaceFinish,
+        String usage,
+        String thickness,
+        String diameter,
+        String width,
+        String quantity,
+        String price,
+        String unitMinWeight,
+        String unitMaxWeight,
+        String edge
+    ) {
+        this.offerSheet = offerSheet;
+        this.product = product;
+        this.specification = specification;
+        this.surfaceFinish = surfaceFinish;
+        this.usage = usage;
+        this.thickness = thickness;
+        this.diameter = diameter;
+        this.width = width;
+        this.quantity = quantity;
+        this.price = price;
+        this.unitMinWeight = unitMinWeight;
+        this.unitMaxWeight = unitMaxWeight;
+        this.edge = edge;
+    }
+
+    public void updateReceipt(
+        String product,
+        String specification,
+        String surfaceFinish,
+        String usage,
+        String thickness,
+        String diameter,
+        String width,
+        String quantity,
+        String price,
+        String unitMinWeight,
+        String unitMaxWeight,
+        String edge
+    ) {
+        this.product = product;
+        this.specification = specification;
+        this.surfaceFinish = surfaceFinish;
+        this.usage = usage;
+        this.thickness = thickness;
+        this.diameter = diameter;
+        this.width = width;
+        this.quantity = quantity;
+        this.price = price;
+        this.unitMinWeight = unitMinWeight;
+        this.unitMaxWeight = unitMaxWeight;
+        this.edge = edge;
+    }
+
+    public void deleteReceipt(){
+        this.isActivated = false;
+    }
+}
