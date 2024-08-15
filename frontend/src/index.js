3a6113f68e5798dbcf246b538892fcbd05e5c112
heejinssss <heejin.dev33@gmail.com>
2024-08-16 01:56:21 +0900

    pbs-176--chore: Recoil 전역 변수 로컬 스토리지 저장

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { RecoilRoot, atom, selector } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 // Atom 생성 함수
 const createAtom = (key, defaultValue = '') => {
     return atom({
         key: key,
         default: defaultValue,
+        effects_UNSTABLE: [persistAtom],
     });
 };
 
@@ -27,12 +31,16 @@ export const authByRole = createAtom('authByRole');
 export const userName = createAtom('userName');
 export const userEmail = createAtom('userEmail');
 export const userPassword = createAtom('userPassword');
+export const joinErrorMsg = createAtom('joinErrorMsg');
+export const loginErrorMsg = createAtom('loginErrorMsg');
 
 // [Recoil] Selector로 변수 Get 호출
 export const getAuthByRole = createSelector('getAuthByRole', authByRole);
 export const getUserName = createSelector('getUserName', userName);
 export const getUserEmail = createSelector('getUserEmail', userEmail);
 export const getUserPassword = createSelector('getUserPassword', userPassword);
+export const getJoinErrorMsg = createAtom('getJoinErrorMsg', joinErrorMsg);
+export const getLoginErrorMsg = createAtom('getLoginErrorMsg', loginErrorMsg);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
