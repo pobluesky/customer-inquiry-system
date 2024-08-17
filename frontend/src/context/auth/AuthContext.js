2b00716909d8eb38c90e135b61f9fda9fcf9530c
Jiyoon Bae <88182667+Jiyoongrace@users.noreply.github.com>
2024-08-17 17:36:33 +0900

    pbs-166--feat: 새로고침 해야만 userId 담기는 문제(이전 role 때와 동일) => setUserId 전역 Context 추가

--- frontend/src/context/auth/AuthContext.js
+++ frontend/src/context/auth/AuthContext.js
@@ -23,6 +23,7 @@ export const AuthProvider = ({ children }) => {
         const currentUserId = getCookie('userId');
 
         if (token) {
+            console.log("aaa")
             setToken(token);
             setDidLogin(true);
             setRole(currentUserRole);
