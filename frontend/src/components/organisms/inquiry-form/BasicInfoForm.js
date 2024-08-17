499fbec7ff936b1efaef36b78bbd6f02ecd15fb2
Jiyoon Bae <88182667+Jiyoongrace@users.noreply.github.com>
2024-08-17 23:15:56 +0900

    pbs-166--feat: select 박스 추가

--- frontend/src/components/organisms/inquiry-form/BasicInfoForm.js
+++ frontend/src/components/organisms/inquiry-form/BasicInfoForm.js
@@ -11,76 +11,90 @@ const BasicInfoForm = () => {
           <ToggleBar title={"기본정보"} isChecked={isChecked} setCheck={setCheck} />
           {isChecked ? (
               <div className={Opend}>
-                <div className={Wrapper}>
+                  <div className={Wrapper}>
 
-                  {/* 1행 */}
-                  <div className={inputWrapper}>
-                    <label>고객사구분</label>
-                    <input type="text" className={_Input}
-                           placeholder="20180829495"/>
-                  </div>
-                  <div className={inputWrapper}>
-                    <label>국가</label>
-                    <input type="text" className={_Input}
-                           placeholder="홍콩"/>
-                  </div>
-                  <div className={inputWrapper}>
-                    <label>판매상사</label>
-                    <input type="text" className={_Input}
-                           placeholder="POA"/>
-                  </div>
+                      {/* 1행 */}
+                      <div className={inputWrapper}>
+                          <label>고객사구분</label>
+                          <input type="text" className={_Input}
+                                 placeholder="20180829495" />
+                      </div>
+                      <div className={inputWrapper}>
+                          <label>국가</label>
+                          <select className={_Input}>
+                              <option value="" disabled selected>선택</option>
+                              <option value="hk">홍콩</option>
+                              <option value="kr">한국</option>
+                              <option value="jp">일본</option>
+                              <option value="cn">중국</option>
+                          </select>
+                      </div>
+                      <div className={inputWrapper}>
+                          <label>판매상사</label>
+                          <input type="text" className={_Input}
+                                 placeholder="POA" />
+                      </div>
 
-                  {/* 2행 */}
-                  <div className={inputWrapper}>
-                    <label>판매계약자</label>
-                    <input type="text" className={_Input}
-                           placeholder="POSCO Asia Co., Ltd."/>
-                  </div>
-                  <div className={inputWrapper}>
-                    <label>Inquiry 유형</label>
-                    <input type="text" className={_Input}
-                           placeholder="품질 + 견적"/>
-                  </div>
-                  <div className={inputWrapper}>
-                    <label>산업분류</label>
-                    <input type="text" className={_Input}
-                           placeholder="자동차산업(Automobile)"/>
-                  </div>
+                      {/* 2행 */}
+                      <div className={inputWrapper}>
+                          <label>판매계약자</label>
+                          <input type="text" className={_Input}
+                                 placeholder="POSCO Asia Co., Ltd." />
+                      </div>
+                      <div className={inputWrapper}>
+                          <label>Inquiry 유형</label>
+                          <select className={_Input}>
+                              <option value="" disabled selected>문의유형</option>
+                              <option value='all'>품질 + 견적</option>
+                              <option value="quality">품질</option>
+                              <option value="sales">견적</option>
+                          </select>
+                      </div>
+                      <div className={inputWrapper}>
+                          <label>산업분류</label>
+                          <input type="text" className={_Input}
+                                 placeholder="자동차산업(Automobile)" />
+                      </div>
 
-                  {/* 3행 */}
-                  <div className={inputWrapper}>
-                    <label>의뢰인명</label>
-                    <input type="text" className={_Input}
-                           placeholder="김세윤"/>
-                  </div>
-                  <div className={inputWrapper}>
-                    <label>의뢰인 E-mail</label>
-                    <input type="text" className={_Input}
-                           placeholder="zhaofeng@posco.net"/>
-                  </div>
-                  <div className={inputWrapper}>
-                    <label>의뢰인 연락처</label>
-                    <input type="text" className={_Input}
-                           placeholder="86-18901251225"/>
-                  </div>
+                      {/* 3행 */}
+                      <div className={inputWrapper}>
+                          <label>의뢰인명</label>
+                          <input type="text" className={_Input}
+                                 placeholder="김세윤" />
+                      </div>
+                      <div className={inputWrapper}>
+                          <label>의뢰인 E-mail</label>
+                          <input type="text" className={_Input}
+                                 placeholder="zhaofeng@posco.net" />
+                      </div>
+                      <div className={inputWrapper}>
+                          <label>의뢰인 연락처</label>
+                          <input type="text" className={_Input}
+                                 placeholder="86-18901251225" />
+                      </div>
 
-                  {/* 4행 */}
-                  <div className={inputWrapper}>
-                    <label>법인코드</label>
-                    <input type="text" className={_Input}
-                           placeholder="(주)포스코"/>
+                      {/* 4행 */}
+                      <div className={inputWrapper}>
+                          <label>법인코드</label>
+                          <input type="text" className={_Input}
+                                 placeholder="(주)포스코" />
+                      </div>
+                      <div className={inputWrapper}>
+                          <label>제품</label>
+                          <select className={_Input}>
+                              <option value="car" disabled selected>자동차</option>
+                              <option value="hotrolled">열연</option>
+                              <option value="coldrolled">냉연</option>
+                              <option value="thickplate">후판</option>
+                              <option value="wirerod">선재</option>
+                          </select>
+                      </div>
+                      <div className={inputWrapper}>
+                          <label>고객요청일</label>
+                          <input type="text" className={_Input}
+                                 placeholder="2024-07-23" />
+                      </div>
                   </div>
-                  <div className={inputWrapper}>
-                    <label>제품</label>
-                    <input type="text" className={_Input}
-                           placeholder="자동차"/>
-                  </div>
-                  <div className={inputWrapper}>
-                    <label>고객요청일</label>
-                    <input type="text" className={_Input}
-                           placeholder="2024-07-23"/>
-                  </div>
-                </div>
               </div>
           ) : (
               ''
