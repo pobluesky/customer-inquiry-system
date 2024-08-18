efeca7a029d474f1833ea5115e6053a9846b909b
Jiyoon Bae <88182667+Jiyoongrace@users.noreply.github.com>
2024-08-18 21:11:54 +0900

    pbs-166--feat: Inquiry POST API 1차 완료

--- frontend/src/apis/api/inquiry.js
+++ frontend/src/apis/api/inquiry.js
@@ -1,5 +1,21 @@
 import axiosInstance from "../utils/axiosInstance";
 
+const createFormData = (data) => {
+  const formData = new FormData();
+
+  Object.keys(data).forEach((key) => {
+    if (key === 'files') {
+      // 파일 데이터는 직접 append
+      formData.append('files', data.files);
+    } else {
+      // 일반 텍스트 데이터는 그냥 append
+      formData.append(key, data[key]);
+    }
+  });
+
+  return formData;
+};
+
 export const getInquiry = async (userId) => {
   try {
     const response = await axiosInstance.get(`/customers/inquiries/${userId}`);
@@ -9,6 +25,23 @@ export const getInquiry = async (userId) => {
 
   } catch (error) {
     console.error('Error fetching Inquiry:', error);
+    throw error;
+  }
+};
+
+// 고객사 Inquiry 등록
+export const postInquiry = async (userId, inquiryData) => {
+  try {
+    const formData = createFormData(inquiryData);
+
+    const response = await axiosInstance.post(
+        `/customers/inquiries/${userId}`,
+        formData,
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error('Error posting inquiry:', error);
     throw error;
   }
 };
--- frontend/src/pages/inq-form/CustomerInqForm.js
+++ frontend/src/pages/inq-form/CustomerInqForm.js
@@ -7,7 +7,7 @@ import {
     AdditionalRequestForm,
     FileForm,
 } from '../../components/organisms/inquiry-form';
-import { postInquiry, postLineItems } from '../../apis/api/inquiry';
+import { postInquiry } from '../../apis/api/inquiry';
 import { useAuth } from '../../hooks/useAuth';
 
 function CustomerInqForm() {
@@ -24,7 +24,7 @@ function CustomerInqForm() {
         progress: 'RECEIPT',
         customerRequestDate: '',
         additionalRequests: '',
-        files: [],
+        files: '',
         responseDeadline: '',
         elapsedDays: '',
         isActivated: true,
@@ -38,46 +38,24 @@ function CustomerInqForm() {
         }));
     };
 
-    const [lineItems, setLineItems] = useState([]);
-
-    // 라인아이템 변경 핸들러
-    const handleLineItemsChange = (newLineItems) => {
-        setLineItems(prevLineItems => {
-            if (JSON.stringify(prevLineItems) !== JSON.stringify(newLineItems)) {
-                return newLineItems;
-            }
-            return prevLineItems;
-        });
-    };
-
     // Inquiry 등록 버튼 클릭 핸들러
     const handleSubmit = async () => {
         try {
             const response = await postInquiry(userId, formData);
             console.log('Inquiry submitted successfully:', response);
-
-            const inquiryId = response.data.inquiryId;
-            console.log('Inquiry ID:', inquiryId);
-
-            console.log('Line items');
-            const lineItemsResponse = await postLineItems(inquiryId, lineItems);
-            console.log('Line items submitted successfully:', lineItemsResponse);
         } catch (error) {
             console.error('Error submitting inquiry:', error);
         }
     };
 
     console.log(formData);
-    console.log(lineItems);
 
     return (
         <div>
             <InqPath largeCategory={'Inquiry'} mediumCategory={'Inquiry 조회'} />
             <RequestBar requestBarTitle={"Inquiry 등록"} role={"customer"} onSubmit={handleSubmit} />
             <InquiryNewForm formData={formData} handleFormDataChange={handleFormDataChange} />
-            <InquiryHistoryForm userId={userId}
-                                productType={formData.productType}
-                                onLineItemsChange={handleLineItemsChange} />
+            <InquiryHistoryForm userId={userId} />
             <AdditionalRequestForm formData={formData} handleFormDataChange={handleFormDataChange} />
             <FileForm fileForm={"파일첨부"} formData={formData} handleFormDataChange={handleFormDataChange} />
         </div>
