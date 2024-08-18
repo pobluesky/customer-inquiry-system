361c0103aa2f85bc625d4517d67a6bbd250a6dd7
Jiyoon Bae <88182667+Jiyoongrace@users.noreply.github.com>
2024-08-18 22:59:35 +0900

    pbs-166--feat: Inquiry POST API 2차 완료

--- frontend/src/pages/inq-form/CustomerInqForm.js
+++ frontend/src/pages/inq-form/CustomerInqForm.js
@@ -7,7 +7,7 @@ import {
     AdditionalRequestForm,
     FileForm,
 } from '../../components/organisms/inquiry-form';
-import { postInquiry } from '../../apis/api/inquiry';
+import { postInquiry, postLineItems } from '../../apis/api/inquiry';
 import { useAuth } from '../../hooks/useAuth';
 
 function CustomerInqForm() {
@@ -38,24 +38,49 @@ function CustomerInqForm() {
         }));
     };
 
+    const [lineItems, setLineItems] = useState([]);
+
+    // 라인아이템 변경 핸들러
+    const handleLineItemsChange = (newLineItems) => {
+        setLineItems(prevLineItems => {
+            if (JSON.stringify(prevLineItems) !== JSON.stringify(newLineItems)) {
+                return newLineItems;
+            }
+            return prevLineItems;
+        });
+    };
+
     // Inquiry 등록 버튼 클릭 핸들러
     const handleSubmit = async () => {
         try {
+            // Post the inquiry data
             const response = await postInquiry(userId, formData);
             console.log('Inquiry submitted successfully:', response);
+
+            // Assuming the response contains the inquiry ID
+            const inquiryId = response.data.inquiryId;
+            console.log('Inquiry ID:', inquiryId);
+
+            // Post the line items data
+            console.log('Line items');
+            const lineItemsResponse = await postLineItems(inquiryId, lineItems);
+            console.log('Line items submitted successfully:', lineItemsResponse);
         } catch (error) {
             console.error('Error submitting inquiry:', error);
         }
     };
 
     console.log(formData);
+    console.log(lineItems);
 
     return (
         <div>
             <InqPath largeCategory={'Inquiry'} mediumCategory={'Inquiry 조회'} />
             <RequestBar requestBarTitle={"Inquiry 등록"} role={"customer"} onSubmit={handleSubmit} />
             <InquiryNewForm formData={formData} handleFormDataChange={handleFormDataChange} />
-            <InquiryHistoryForm userId={userId} />
+            <InquiryHistoryForm userId={userId}
+                                productType={formData.productType}
+                                onLineItemsChange={handleLineItemsChange} />
             <AdditionalRequestForm formData={formData} handleFormDataChange={handleFormDataChange} />
             <FileForm fileForm={"파일첨부"} formData={formData} handleFormDataChange={handleFormDataChange} />
         </div>
