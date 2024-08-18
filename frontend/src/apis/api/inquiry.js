2a45be84988de319bae69ac35e2104a80e7e5d7c
Jiyoon Bae <88182667+Jiyoongrace@users.noreply.github.com>
2024-08-18 14:47:04 +0900

    pbs-166--feat: Inquiry Api 연동 start

--- frontend/src/apis/api/inquiry.js
+++ frontend/src/apis/api/inquiry.js
@@ -1,21 +1,5 @@
 import axiosInstance from "../utils/axiosInstance";
 
-const createFormData = (data) => {
-  const formData = new FormData();
-
-  Object.keys(data).forEach((key) => {
-    if (key === 'files') {
-      // 파일 데이터는 직접 append
-      formData.append('files', data.files);
-    } else {
-      // 일반 텍스트 데이터는 그냥 append
-      formData.append(key, data[key]);
-    }
-  });
-
-  return formData;
-};
-
 export const getInquiry = async (userId) => {
   try {
     const response = await axiosInstance.get(`/customers/inquiries/${userId}`);
@@ -25,40 +9,6 @@ export const getInquiry = async (userId) => {
 
   } catch (error) {
     console.error('Error fetching Inquiry:', error);
-    throw error;
-  }
-};
-
-// 고객사 Inquiry 등록
-export const postInquiry = async (userId, inquiryData) => {
-  try {
-    const formData = createFormData(inquiryData);
-
-    const response = await axiosInstance.post(
-        `/customers/inquiries/${userId}`,
-        formData,
-    );
-    console.log(inquiryData);
-    console.log(response);
-    return response.data;
-  } catch (error) {
-    console.error('Error posting inquiry:', error);
-    throw error;
-  }
-};
-
-// 고객사 LineItem 등록
-export const postLineItems = async (inquiryId, lineItemData) => {
-  try {
-    const response = await axiosInstance.post(
-        `/line-items/${inquiryId}`,
-        lineItemData,
-    );
-    console.log(lineItemData);
-    console.log(response);
-    return response.data;
-  } catch (error) {
-    console.error('Error posting line item:', error);
     throw error;
   }
 };
