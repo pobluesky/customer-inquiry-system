ad7c077aaeb1208fad40e3f1142a0b2c79f8a234
heejinssss <heejin.dev33@gmail.com>
2024-08-20 12:38:11 +0900

    pbs-189--feat: Merge 중 누락된 차트 파일 추가

--- frontend/src/pages/dashboard/DashBoard.js
+++ frontend/src/pages/dashboard/DashBoard.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import BarChart from '../../components/organisms/BarChart';
 import PieChart from '../../components/organisms/PieChart';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 function DashBoard() {
-<<<<<<< Updated upstream
-    return (
-        <>
-            <BarChart />
-=======
-    const data = [{productType: '자동차', progress: []]},
-                  {productType: '자동차', progress: '1차 검토'},
-                  {productType: '자동차', progress: '접수'}]
+    const data = [
+        { productType: '자동차', progress: [] },
+        { productType: '자동차', progress: '1차 검토' },
+        { productType: '자동차', progress: '접수' },
+    ];
 
-    const dataSample = [{productType: '자동차', progress: [100, 200, 300, 400]}, {productType: '열연', progress: [400, 300, 200, 100]}];
+    const dataSample = [
+        { productType: '자동차', progress: [100, 200, 300, 400] },
+        { productType: '열연', progress: [400, 300, 200, 100] },
+    ];
 
     return (
         <>
             <Header login={true} inq={false} voc={false} dashboard={true} />
-            <HorizontalBarChart data={data}/>
+            <HorizontalBarChart data={data} />
             <VerticalBarChart />
->>>>>>> Stashed changes
             <PieChart />
             <div style={{ width: 200, height: 200 }}>
                 <CircularProgressbar value={66} />
