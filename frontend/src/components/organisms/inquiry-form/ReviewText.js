278c471fcc0649de50f8c80699d072f1fc6e6c55
Jiyoon Bae <88182667+Jiyoongrace@users.noreply.github.com>
2024-08-23 17:52:04 +0900

    pbs-166--feat: quality api

---
+++ frontend/src/components/organisms/inquiry-form/ReviewText.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ListLabel from "../../mocules/ListLabel";
+import {ReviewWrapper, _TextArea} from "../../../assets/css/Form.css";
+
+const ReviewText = ({ title, width, height, content, handleFormDataChange }) => {
+  return (
+      <div>
+        <ReviewWrapper>
+          <ListLabel margin="0 0 0 20px" color="#ffffff"
+                     width={width} padding="8px 0 0 5px"
+                     backgroundColor="#03507D" content={title}/>
+          <div>
+              <textarea
+                  value={content}
+                  className={_TextArea} style={{
+                  borderRadius: '0 12px 12px 12px',
+                  height: height,
+                  }}
+                  onChange={(e) =>
+                      handleFormDataChange(
+                          'qualityComments',
+                          e.target.value
+                      )}/>
+          </div>
+        </ReviewWrapper>
+      </div>
+  );
+};
+
+export default ReviewText;
