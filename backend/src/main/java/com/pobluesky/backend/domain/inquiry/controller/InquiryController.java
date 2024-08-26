08fb088aec71e1988c7bc761a8b654da2ad087ed
born-A <93516595+born-A@users.noreply.github.com>
2024-08-26 11:01:59 +0900

    pbs-223--refactor: inquiry 상태 바로 변경

--- backend/src/main/java/com/pobluesky/backend/domain/inquiry/controller/InquiryController.java
+++ backend/src/main/java/com/pobluesky/backend/domain/inquiry/controller/InquiryController.java
@@ -1,7 +1,6 @@
 package com.pobluesky.backend.domain.inquiry.controller;
 
 import com.pobluesky.backend.domain.inquiry.dto.request.InquiryCreateRequestDTO;
-import com.pobluesky.backend.domain.inquiry.dto.request.InquiryProgressUpdateRequestDTO;
 import com.pobluesky.backend.domain.inquiry.dto.request.InquiryUpdateRequestDTO;
 import com.pobluesky.backend.domain.inquiry.dto.response.InquiryProgressResponseDTO;
 import com.pobluesky.backend.domain.inquiry.dto.response.InquiryResponseDTO;
@@ -32,7 +31,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -267,10 +265,9 @@ public class InquiryController {
     @Operation(summary = "담당자 Inquiry 상태 업데이트")
     public ResponseEntity<InquiryProgressResponseDTO> updateInquiryProgress(
         @RequestHeader("Authorization") String token,
-        @PathVariable Long inquiryId,
-        @RequestBody InquiryProgressUpdateRequestDTO requestDTO
+        @PathVariable Long inquiryId
     ) {
-        InquiryProgressResponseDTO response = inquiryService.updateInquiryProgress(token, inquiryId, requestDTO);
+        InquiryProgressResponseDTO response = inquiryService.updateInquiryProgress(token, inquiryId);
         return ResponseEntity.ok(response);
     }
 }
