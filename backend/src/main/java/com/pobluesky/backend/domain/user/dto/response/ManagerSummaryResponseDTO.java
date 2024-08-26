66f52d20f7b6ea97b1be2a6e05b522d65b0d0d44
born-A <93516595+born-A@users.noreply.github.com>
2024-08-26 16:55:46 +0900

    pbs-226--feat : inquiry 담당자 resopnse

---
+++ backend/src/main/java/com/pobluesky/backend/domain/user/dto/response/ManagerSummaryResponseDTO.java
@@ -0,0 +1,22 @@
+package com.pobluesky.backend.domain.user.dto.response;
+
+import com.pobluesky.backend.domain.user.entity.Manager;
+
+import lombok.Builder;
+
+@Builder
+public record ManagerSummaryResponseDTO(
+    Long userId,
+    String name
+) {
+
+    public static ManagerSummaryResponseDTO from(Manager manager) {
+        if (manager == null) return null;
+        else {
+            return ManagerSummaryResponseDTO.builder()
+                .userId(manager.getUserId())
+                .name(manager.getName())
+                .build();
+        }
+    }
+}
