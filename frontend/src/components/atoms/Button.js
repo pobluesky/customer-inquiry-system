06427cf7f0e684a52d0c5c285236a98bf3638eea
heejinssss <heejin.dev33@gmail.com>
2024-08-27 16:20:28 +0900

    pbs-229--feat: ref Error 수정

--- frontend/src/components/atoms/Button.js
+++ frontend/src/components/atoms/Button.js
@@ -1,74 +1,80 @@
 import React from 'react';
 
-function Button({
-    btnName,
-    onClick,
-    onKeyDown,
-    width,
-    height,
-    margin,
-    padding,
-    backgroundColor,
-    textColor,
-    fontSize,
-    fontWeight,
-    border,
-    borderRadius,
-    boxShadow,
-    float,
-    cursor,
-    alignSelf,
-    alignItems,
-    justifySelf,
-    justifyContent,
-    display,
-    imgSrc,
-    imgAlt,
-    imgWidth,
-    imgHeight,
-    imgMargin,
-    imgOnClick,
-}) {
-    return (
-        <button
-            onClick={onClick}
-            onKeyDown={onKeyDown}
-            style={{
-                width,
-                height,
-                margin,
-                padding,
-                backgroundColor,
-                color: textColor,
-                fontSize,
-                fontWeight,
-                border,
-                borderRadius,
-                boxShadow,
-                float,
-                cursor: cursor || 'pointer',
-                alignSelf,
-                alignItems,
-                justifySelf,
-                justifyContent,
-                display,
-            }}
-        >
-            {imgSrc && (
-                <img
-                    src={imgSrc}
-                    alt={imgAlt}
-                    style={{
-                        width: imgWidth,
-                        height: imgHeight,
-                        margin: imgMargin,
-                    }}
-                    onClick={imgOnClick}
-                />
-            )}
-            {btnName}
-        </button>
-    );
-}
+const Button = React.forwardRef(
+    (
+        {
+            btnName,
+            onClick,
+            onKeyDown,
+            width,
+            height,
+            margin,
+            padding,
+            backgroundColor,
+            textColor,
+            fontSize,
+            fontWeight,
+            border,
+            borderRadius,
+            boxShadow,
+            float,
+            cursor,
+            alignSelf,
+            alignItems,
+            justifySelf,
+            justifyContent,
+            display,
+            imgSrc,
+            imgAlt,
+            imgWidth,
+            imgHeight,
+            imgMargin,
+            imgOnClick,
+        },
+        ref,
+    ) => {
+        return (
+            <button
+                ref={ref}
+                onClick={onClick}
+                onKeyDown={onKeyDown}
+                style={{
+                    width,
+                    height,
+                    margin,
+                    padding,
+                    backgroundColor,
+                    color: textColor,
+                    fontSize,
+                    fontWeight,
+                    border,
+                    borderRadius,
+                    boxShadow,
+                    float,
+                    cursor: cursor || 'pointer',
+                    alignSelf,
+                    alignItems,
+                    justifySelf,
+                    justifyContent,
+                    display,
+                }}
+            >
+                {imgSrc && (
+                    <img
+                        src={imgSrc}
+                        alt={imgAlt}
+                        style={{
+                            width: imgWidth,
+                            height: imgHeight,
+                            margin: imgMargin,
+                        }}
+                        onClick={imgOnClick}
+                    />
+                )}
+                {btnName}
+            </button>
+        );
+    },
+);
 
 export default Button;
