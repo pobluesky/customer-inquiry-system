e88ef8d17ec6ba3138a210225dc53e8466bea917
heejinssss <heejin.dev33@gmail.com>
2024-08-29 17:32:16 +0900

    pbs-229--feat: Chart Library 설치 + 환경 설정

--- frontend/src/pages/dashboard/DashBoard.js
+++ frontend/src/pages/dashboard/DashBoard.js
@@ -1,30 +1,21 @@
 import React from 'react';
-import HorizontalBarChart from '../../components/organisms/HorizontalBarChart';
-import VerticalBarChart from '../../components/organisms/BarChart'
-import PieChart from '../../components/organisms/PieChart';
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import InquiryMonthlyFilledOrderChart from '../../components/organisms/InquiryMonthlyFilledOrderChart';
+import InquiryProgressChart from '../../components/organisms/InquiryProgressChart';
+import InquiryFilledOrderCountChart from '../../components/organisms/InquiryFilledOrderCountChart';
+import InquiryProductProgressChart from '../../components/organisms/InquiryProductProgressChart'
+// import HorizontalBarChart from '../../components/organisms/HorizontalBarChart';
+// import VerticalBarChart from '../../components/organisms/BarChart'
+// import PieChart from '../../components/organisms/PieChart';
+// import { CircularProgressbar } from 'react-circular-progressbar';
+// import 'react-circular-progressbar/dist/styles.css';
 
 export default function DashBoard() {
-    const data = [
-        { productType: '자동차', progress: [] },
-        { productType: '자동차', progress: '1차 검토' },
-        { productType: '자동차', progress: '접수' },
-    ];
-
-    const dataSample = [
-        { productType: '자동차', progress: [100, 200, 300, 400] },
-        { productType: '열연', progress: [400, 300, 200, 100] },
-    ];
-
     return (
         <>
-            <HorizontalBarChart data={data} />
-            <VerticalBarChart />
-            <PieChart />
-            <div style={{ width: 200, height: 200 }}>
-                <CircularProgressbar value={66} />
-            </div>
+            <InquiryMonthlyFilledOrderChart />
+            <InquiryProgressChart />
+            <InquiryFilledOrderCountChart />
+            <InquiryProductProgressChart />
         </>
     );
 }
