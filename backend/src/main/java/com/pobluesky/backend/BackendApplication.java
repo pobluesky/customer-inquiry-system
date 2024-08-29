56df851d139e8b954e06909fed1ae197ca51b487
parlyresk <parlyresk325@gmail.com>
2024-08-29 13:51:13 +0900

    pbs-243--feat: build Eureka Server

--- backend/src/main/java/com/pobluesky/backend/BackendApplication.java
+++ backend/src/main/java/com/pobluesky/backend/BackendApplication.java
@@ -4,6 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
