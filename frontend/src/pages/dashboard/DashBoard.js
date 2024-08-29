5f99c1290dcef9006e8b82873fe16b08edde1ead
heejinssss <heejin.dev33@gmail.com>
2024-08-29 17:51:12 +0900

    pbs-263--chore: 패키지 + 주석문 정리

--- frontend/src/pages/dashboard/DashBoard.js
+++ frontend/src/pages/dashboard/DashBoard.js
@@ -3,11 +3,6 @@ import InquiryMonthlyFilledOrderChart from '../../components/organisms/InquiryMo
 import InquiryProgressChart from '../../components/organisms/InquiryProgressChart';
 import InquiryFilledOrderCountChart from '../../components/organisms/InquiryFilledOrderCountChart';
 import InquiryProductProgressChart from '../../components/organisms/InquiryProductProgressChart'
-// import HorizontalBarChart from '../../components/organisms/HorizontalBarChart';
-// import VerticalBarChart from '../../components/organisms/BarChart'
-// import PieChart from '../../components/organisms/PieChart';
-// import { CircularProgressbar } from 'react-circular-progressbar';
-// import 'react-circular-progressbar/dist/styles.css';
 
 export default function DashBoard() {
     return (
