924b92cfea759061acc10b4a4a92d30a1b91771a
born-A <ra_ari@naver.com>
2024-09-02 15:42:57 +0900

    pbs-321--feat : mobile question api, dto

---
+++ backend/src/main/java/com/pobluesky/backend/domain/question/dto/response/MobileQuestionSummaryResponseDTO.java
@@ -0,0 +1,29 @@
+package com.pobluesky.backend.domain.question.dto.response;
+
+import com.pobluesky.backend.domain.question.entity.Question;
+import lombok.Builder;
+
+@Builder
+public record MobileQuestionSummaryResponseDTO (
+
+        Long questionId,
+
+        String customer,
+
+        String title,
+
+        String status,
+
+        String type
+) {
+    public static MobileQuestionSummaryResponseDTO from(Question question) {
+
+        return MobileQuestionSummaryResponseDTO.builder()
+                .questionId(question.getQuestionId())
+                .customer(question.getCustomer().getCustomerName())
+                .title(question.getTitle())
+                .status(question.getStatus().getStatus())
+                .type(question.getType().getType())
+                .build();
+    }
+}
