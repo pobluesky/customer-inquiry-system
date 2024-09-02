807b1c7e3eba4082fae04d21bbbf7d3c9cf74894
born-A <ra_ari@naver.com>
2024-09-02 15:37:41 +0900

    pbs-321--feat: question repo isActivated 필터링 조회

--- backend/src/main/java/com/pobluesky/backend/domain/question/repository/QuestionRepository.java
+++ backend/src/main/java/com/pobluesky/backend/domain/question/repository/QuestionRepository.java
@@ -1,11 +1,20 @@
 package com.pobluesky.backend.domain.question.repository;
 
 import com.pobluesky.backend.domain.question.entity.Question;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long>, QuestionRepositoryCustom {
+
+    @Query("SELECT q FROM Question q WHERE q.questionId = :questionId AND q.isActivated = true")
+    Optional<Question> findActiveQuestionByQuestionId(Long questionId);
+
+    @Query("SELECT q FROM Question q WHERE q.isActivated = true")
+    List<Question> findActiveQuestions();
 }
