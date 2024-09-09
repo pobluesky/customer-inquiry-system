d6a4978cf9b033ce2b194f57f1c3596e4a2e0240
mmmtobezip <mmmszip@gmail.com>
2024-09-09 23:53:05 +0900

    pbs-306--feat: Collaboration 게시판 수정 RequestDTO 생성

---
+++ backend/src/main/java/com/pobluesky/backend/domain/collaboration/dto/request/CollaborationModifyRequestDTO.java
@@ -0,0 +1,10 @@
+package com.pobluesky.backend.domain.collaboration.dto.request;
+
+public record CollaborationModifyRequestDTO(
+    Long colReqId,
+    Long colResId,
+    String colContents,
+    Boolean isAccepted,
+    String colReply
+) {
+}
