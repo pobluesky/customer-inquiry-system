63361e9dc9ff6e3b0a98165d9f77a28ad190ceb4
mmmtobezip <mmmszip@gmail.com>
2024-09-09 23:53:34 +0900

    pbs-306--feat: 요청 매니저 검증 에러 코드 추가

--- backend/src/main/java/com/pobluesky/backend/global/error/ErrorCode.java
+++ backend/src/main/java/com/pobluesky/backend/global/error/ErrorCode.java
@@ -74,6 +74,7 @@ public enum ErrorCode {
     COLLABORATION_STATUS_REFUSED(HttpStatus.INTERNAL_SERVER_ERROR, "C0004", "이미 거절된 협업입니다."),
     COLLABORATION_INFO_MISMATCH(HttpStatus.INTERNAL_SERVER_ERROR, "C0005", "일치하지 않은 협업 정보입니다."),
     RESMANAGER_NOT_MACHED(HttpStatus.INTERNAL_SERVER_ERROR, "C0006", "해당 협업의 응답 담당자가 아닙니다."),
+    REQMANAGER_NOT_MACHED(HttpStatus.INTERNAL_SERVER_ERROR, "C0007", "해당 협업의 요청 담당자가 아닙니다."),
 
     // AI
     OCR_PROCESS_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "AI001", "텍스트 추출에 실패했습니다."),
