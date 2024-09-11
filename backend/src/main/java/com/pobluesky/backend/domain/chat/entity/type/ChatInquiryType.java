887ae6989521cb03b1cb68abc9859011748d8a42
mmmtobezip <mmmszip@gmail.com>
2024-09-11 11:19:32 +0900

    pbs-353--feat: chatbot inquiry type 구분

---
+++ backend/src/main/java/com/pobluesky/backend/domain/chat/entity/type/ChatInquiryType.java
@@ -0,0 +1,17 @@
+package com.pobluesky.backend.domain.chat.entity.type;
+
+public enum ChatInquiryType {
+    ORDER("Order", "주문 문의"),
+    PRODUCT("Product", "제품 종합 문의"),
+    REGISTRATION("Registration", "등록 문의"),
+    SITE_USAGE("Site Usage", "사이트 이용 문의"),
+    OTHER("Other", "기타 문의");
+
+    private final String type;
+    private final String name;
+
+    ChatInquiryType(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+}
