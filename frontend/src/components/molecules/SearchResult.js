925183691b3790c01008297fc38e5ad4149d44a1
Jiyoon Bae <88182667+Jiyoongrace@users.noreply.github.com>
2024-09-23 10:38:02 +0900

    pbs-392--feat: Inquiry 등록 버튼 추가

--- frontend/src/components/molecules/SearchResult.js
+++ frontend/src/components/molecules/SearchResult.js
@@ -3,28 +3,35 @@ import {
     _Text,
     _searchCount,
 } from '../../assets/css/Inquiry.css';
-import Button from '../atoms/Button';
+import { Button } from '@mui/material';
 
 const SearchResult = ({ searchResult }) => {
     return (
-        <div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
             <p className={_Text}>
                 검색 결과 : 총&nbsp;
                 <span className={_searchCount}>{searchResult}</span>
                 &nbsp;건
             </p>
-            {/*
             <Button
-                btnName={'엑셀 다운로드'}
-                textColor={'#ffffff'}
-                borderRadius={'17px'}
-                width={'120px'}
-                height={'35px'}
-                fontWeight={'800'}
-                backgroundColor={'#03507D'}
-                border={'none'}
-                margin={'25px 0 0 20px'}
-            />*/}
+                variant="contained"
+                color="secondary"
+                sx={{
+                    marginLeft: '20px',
+                    backgroundColor: '#ffffff',
+                    color: '#03507d',
+                    width: '100px',
+                    height: '40px',
+                    padding: '0',
+                    borderRadius: '10px',
+                    fontWeight: '700',
+                    border: '1px solid #03507d',
+                    '&:hover': {
+                        backgroundColor: '#03507d',
+                        color: '#ffffff',
+                    },
+                }}
+            >Inquiry 등록</Button>
         </div>
     );
 };
