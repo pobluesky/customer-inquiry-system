3de815f8711b892c53b10bdec64bd2f54251dd26
Jiyoon Bae <88182667+Jiyoongrace@users.noreply.github.com>
2024-09-23 11:21:13 +0900

    pbs-392--feat: Inquiry 등록 버튼 role 구분

--- frontend/src/components/molecules/SearchResult.js
+++ frontend/src/components/molecules/SearchResult.js
@@ -4,8 +4,13 @@ import {
     _searchCount,
 } from '../../assets/css/Inquiry.css';
 import { Button } from '@mui/material';
+import { useAuth } from '../../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const SearchResult = ({ searchResult }) => {
+    const { role } = useAuth();
+    const navigate = useNavigate();
+
     return (
         <div style={{ display: 'flex', alignItems: 'center' }}>
             <p className={_Text}>
@@ -13,25 +18,28 @@ const SearchResult = ({ searchResult }) => {
                 <span className={_searchCount}>{searchResult}</span>
                 &nbsp;건
             </p>
-            <Button
-                variant="contained"
-                color="secondary"
-                sx={{
-                    marginLeft: '20px',
-                    backgroundColor: '#ffffff',
-                    color: '#03507d',
-                    width: '100px',
-                    height: '40px',
-                    padding: '0',
-                    borderRadius: '10px',
-                    fontWeight: '700',
-                    border: '1px solid #03507d',
-                    '&:hover': {
-                        backgroundColor: '#03507d',
-                        color: '#ffffff',
-                    },
-                }}
-            >Inquiry 등록</Button>
+            { role === 'customer' && (
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    sx={{
+                        marginLeft: '20px',
+                        backgroundColor: '#ffffff',
+                        color: '#03507d',
+                        width: '100px',
+                        height: '40px',
+                        padding: '0',
+                        borderRadius: '10px',
+                        fontWeight: '700',
+                        border: '1px solid #03507d',
+                        '&:hover': {
+                            backgroundColor: '#03507d',
+                            color: '#ffffff',
+                        },
+                    }}
+                    onClick={() => navigate('/inq-form/customer')}
+                        >Inquiry 등록</Button>
+            )}
         </div>
     );
 };
