887e5a7da76d29e6ff199130ccb0ef9448b94cfb
heejinssss <heejin.dev33@gmail.com>
2024-09-25 09:29:16 +0900

    pbs-398--feat: 담당자 질문 게시 권한 부여

--- frontend/src/pages/voc-form/VocQuestionForm.js
+++ frontend/src/pages/voc-form/VocQuestionForm.js
@@ -1,23 +1,11 @@
-import React, { useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { getCookie } from '../../apis/utils/cookies';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import VocPath from '../../components/atoms/VocPath';
 import QuestionForm from '../../components/templates/QuestionForm';
 
 export default function VocQuestionForm() {
-    const navigate = useNavigate();
-
-    const role = getCookie('userRole');
-
     const location = useLocation();
     const { questionDetail } = location.state || '';
-
-    // 고객사만 질문 등록 가능
-    useEffect(() => {
-        if (role !== 'customer') {
-            navigate('/voc-list/question');
-        }
-    }, []);
 
     return (
         <>
