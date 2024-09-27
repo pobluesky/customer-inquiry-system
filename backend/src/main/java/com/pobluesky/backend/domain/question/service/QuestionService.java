6496484943f90288bcbe1bdb31a4f776ab0cf419
mmmtobezip <mmmszip@gmail.com>
2024-09-27 17:34:57 +0900

    pbs-386--remove: 안쓰는 import문 제거

--- backend/src/main/java/com/pobluesky/backend/domain/question/service/QuestionService.java
+++ backend/src/main/java/com/pobluesky/backend/domain/question/service/QuestionService.java
@@ -32,7 +32,6 @@ import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
