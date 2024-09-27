d73a6c21355d5ebfd786c8466ca3227a038d554a
mmmtobezip <mmmszip@gmail.com>
2024-09-27 17:28:40 +0900

    pbs-386--add: 테스트 api 추가

--- backend/src/main/java/com/pobluesky/backend/domain/chat/controller/ChatController.java
+++ backend/src/main/java/com/pobluesky/backend/domain/chat/controller/ChatController.java
@@ -1,17 +1,22 @@
 package com.pobluesky.backend.domain.chat.controller;
 
 import com.pobluesky.backend.domain.chat.service.ChatbotService;
+import com.pobluesky.backend.domain.chat.service.OcrService;
+import com.pobluesky.backend.global.util.ResponseFactory;
 import com.pobluesky.backend.global.util.model.JsonResult;
 import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,6 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
     private final ChatbotService chatbotService;
+
+    private final OcrService ocrService;
 
     @PostMapping("/send")
     @Operation(summary = "유저 메시지에 대한 Chatbot 응답 제공")
@@ -29,5 +36,25 @@ public class ChatController {
         JsonResult<?> response = chatbotService.processChatMessage(token, userMessage);
 
         return ResponseEntity.ok(response);
+    }
+
+//    @PostMapping("/upload-excel")
+//    public ResponseEntity<JsonResult> processExcel(
+//        @RequestParam("file") MultipartFile file
+//    ) {
+//        //String imagePath = excelToImageService.convertExcelToImage(file);
+//
+//        return ResponseEntity.status(HttpStatus.OK)
+//            .body(ResponseFactory.getSuccessJsonResult(imagePath));
+//    }
+
+    @PostMapping("/detect-text")
+    public ResponseEntity<JsonResult> processPdf(
+        @RequestParam("file") MultipartFile file
+    ) {
+        List<String> textResults = ocrService.processFileAndDetectText(file);
+
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ResponseFactory.getSuccessJsonResult(textResults));
     }
 }
