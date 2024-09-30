2b6a5732a1178f478cd9ef8b02f0a016472967fe
parlyresk <parlyresk325@gmail.com>
2024-09-30 17:35:39 +0900

    pbs-467--feat: inquiry

--- backend/src/main/java/com/pobluesky/backend/domain/inquiry/service/InquiryService.java
+++ backend/src/main/java/com/pobluesky/backend/domain/inquiry/service/InquiryService.java
@@ -335,13 +335,15 @@ public class InquiryService {
         String fileName = inquiry.getFileName();
         String filePath = inquiry.getFilePath();
 
-        if (file != null) {
+        boolean isFileDeleted = inquiryUpdateRequestDTO.isFileDeleted() != null && inquiryUpdateRequestDTO.isFileDeleted();
+
+        if (isFileDeleted) {
+            fileName = null;
+            filePath = null;
+        } else if (file != null) {
             FileInfo fileInfo = fileService.uploadFile(file);
             fileName = fileInfo.getOriginName();
             filePath = fileInfo.getStoredFilePath();
-        }else {
-            fileName = null;
-            filePath = null;
         }
 
         lineItemService.deleteLineItemsByInquiry(inquiry);
